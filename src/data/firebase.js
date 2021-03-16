860ed3608b1bea7f0dfdc0b2eb2b5bfafd296a51
Daniel <daniel.fiola@outlook.com>
2021-03-16 00:15:40 -0400

    remise

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -4,7 +4,14 @@ import 'firebase/firestore';
 /******* Ex#3 - Étape B ********************************/ 
 // Ajouter votre objet de configuration Firebase
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyB2Bc1D60tFsIfKtxyrCeZ7n3hNG7Gc2Uc",
+  authDomain: "panier-achat.firebaseapp.com",
+  databaseURL: "https://panier-achat-default-rtdb.firebaseio.com",
+  projectId: "panier-achat",
+  storageBucket: "panier-achat.appspot.com",
+  messagingSenderId: "522134652630",
+  appId: "1:522134652630:web:c64982ee579bd85f912b77",
+  measurementId: "G-7ZBKRL9T3J"
 };
 
 // Initialiser Firebase
@@ -16,3 +23,4 @@ if(!fbAppli.apps.length) {
 const bd = fbAppli.firestore();
 /******* Ex#3 - Étape C ********************************/ 
 // Exporter (par défaut) la référence à "bd" pour pouvoir l'utiliser dans le composant ListeProduits
+export default bd;
